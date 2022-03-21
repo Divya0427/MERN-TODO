a30546b3b5f0e530a74472486a702721e6fb5420
DivyaUI <divyabhargavi.u@gmail.com>
2022-03-21 19:09:24 +0530

    Added styles and markup changes for the form

--- client/src/pages/NewTodoPage.js
+++ client/src/pages/NewTodoPage.js
@@ -23,65 +23,71 @@ const NewTodoPage = () => {
         <div>
             <form onSubmit={handleSubmit(onSubmit)}>
                 
-                <label htmlFor="title">Title</label>
+                <label htmlFor="title">Title*</label>
+                <span>
+                    <input
+                    id="title"
+                    placeholder="Title"
+                    aria-invalid={errors.title ? "true" : "false"}
+                    {...register('title', { required: true, maxLength: 50 })}
+                    />
 
-                <input
-                id="title"
-                placeholder="Title"
-                aria-invalid={errors.title ? "true" : "false"}
-                {...register('title', { required: true, maxLength: 50 })}
-                />
-
-                {errors.title && errors.title.type === "required" && (
-                <span className={classes.error} role="alert">Title is required</span>
-                )}
-                {errors.title && errors.title.type === "maxLength" && (
-                <span className={classes.error} role="alert">Max length exceeded</span>
-                )}
-
-                <label htmlFor="description">Description</label>
-
-                <input
-                id="description"
-                placeholder="Description"
-                aria-invalid={errors.description ? "true" : "false"}
-                {...register('description', { required: true, maxLength: 100 })}
-                />
+                    {errors.title && errors.title.type === "required" && (
+                    <span className={classes.error} role="alert">Title is required</span>
+                    )}
+                    {errors.title && errors.title.type === "maxLength" && (
+                    <span className={classes.error} role="alert">Max length exceeded</span>
+                    )}
+                </span>
                 
-                {errors.description && errors.description.type === "required" && (
-                <span className={classes.error} role="alert">Description is required</span>
-                )}
-                {errors.description && errors.description.type === "maxLength" && (
-                <span className={classes.error} role="alert">Max length exceeded</span>
-                )}
+                <label htmlFor="description">Description*</label>
+                <span>
+                    <input
+                    id="description"
+                    placeholder="Description"
+                    aria-invalid={errors.description ? "true" : "false"}
+                    {...register('description', { required: true, maxLength: 100 })}
+                    />
+                    
+                    {errors.description && errors.description.type === "required" && (
+                    <span className={classes.error} role="alert">Description is required</span>
+                    )}
+                    {errors.description && errors.description.type === "maxLength" && (
+                    <span className={classes.error} role="alert">Max length exceeded</span>
+                    )}
+                </span>
                 
-                <label htmlFor="status">Status</label>
-                <select name="status" {...register("status", {
-                    required: "select Status"
-                    })}
-                >
-                    <option value="">Select...</option>
-                    <option value="A">status A</option>
-                    <option value="B">status B</option>
-                </select>
+                <label htmlFor="status">Status*</label>
+                <span>
+                    <select name="status" {...register("status", {
+                        required: "Please select Status"
+                        })}
+                    >
+                        <option value="">Select...</option>
+                        <option value="A">status A</option>
+                        <option value="B">status B</option>
+                    </select>
 
-                {errors.status && (
-                    <p className={classes.error}> {errors.status.message}</p>
-                )}
+                    {errors.status && (
+                        <span className={classes.error}> {errors.status.message}</span>
+                    )}
+                </span>
                 
-                <label htmlFor="priority">Priority</label>
-                <select name="priority" {...register("priority", {
-                    required: "select Priority"
-                    })}
-                >
-                    <option value="">Select...</option>
-                    <option value="A">priority A</option>
-                    <option value="B">priority B</option>
-                </select>
+                <label htmlFor="priority">Priority*</label>
+                <span>
+                    <select name="priority" {...register("priority", {
+                        required: "Please select Priority"
+                        })}
+                    >
+                        <option value="">Select...</option>
+                        <option value="A">priority A</option>
+                        <option value="B">priority B</option>
+                    </select>
 
-                {errors.priority && (
-                    <p className={classes.error}> {errors.priority.message}</p>
-                )}
+                    {errors.priority && (
+                        <span className={classes.error}> {errors.priority.message}</span>
+                    )}
+                </span>
                 <input type="submit" />
             </form>            
         </div>
