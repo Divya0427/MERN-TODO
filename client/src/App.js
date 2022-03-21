bee11962906f658914caf3053ec43a8f7640c12e
DivyaUI <Divya0427@users.noreply.github.com>
2022-03-21 14:19:16 +0530

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './components/Layout/Navigation';
+import AllTodosPage from './pages/AllTodosPage';
+import NewTodoPage from './pages/NewTodoPage';
+import FavoriteTodosPage from './pages/FavoriteTodosPage';
+import PageNotFound from './pages/PageNotFound';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Navigation />
+        <Routes>
+            <Route path='/' element={ <AllTodosPage /> }></Route>
+            <Route path='/new-todo' element={ <NewTodoPage /> }></Route>
+            <Route path='/favorites' element={ <FavoriteTodosPage /> }></Route>
+            <Route path='*' element={ <PageNotFound /> }></Route>
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
